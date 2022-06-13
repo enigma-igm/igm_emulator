963e0880b1c1173aa20f213a58f52af5e1aacb0d
joe <joe@physics.ucsb.edu>
2022-06-14 00:51:05 +0200

    Added a directory.

--- igm_emulator/scripts/grab_models.py
+++ igm_emulator/scripts/grab_models.py
@@ -3,8 +3,12 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 from atropy.table import Table
+from igm_emulator.emulator.utils import lhs
 
 # Routine to randomly subsample a set of correlation function simulations
 
 corr_table = Table.read('/Users/mwolfson/data/corr_table.fits')
-#
+
+seed =12345
+# random number generator
+rng = np.random.default_rng(seed)
