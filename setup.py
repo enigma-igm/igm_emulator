fdc2ebe4086a88f852ddb9a54cedfcf20e96f63e
joe <joe@physics.ucsb.edu>
2022-06-14 00:31:42 +0200

    Edited setup file.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ if path.exists('README.md'):
 
 
 setup(
-    name="highz_qso_redux",
+    name="igm_emulator",
     version='0.0.dev0',
     description="Determining the best description of quasar spectrum",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="https://github.com/enigma-igm/highz_qso_redux",
-    author='Silvia Onorato and more',
+    url="https://github.com/enigma-igm/igm_emulator",
+    author='Linda Jin and friends',
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "numpy",
