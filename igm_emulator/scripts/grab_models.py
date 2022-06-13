62744610dd7a317e19ed68044828918eb1877149
joe <joe@physics.ucsb.edu>
2022-06-14 00:43:32 +0200

    refactoring

---
+++ igm_emulator/scripts/grab_models.py
@@ -0,0 +1,10 @@
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from atropy.table import Table
+
+# Routine to randomly subsample a set of correlation function simulations
+
+corr_table = Table.read('/Users/mwolfson/data/corr_table.fits')
+#
