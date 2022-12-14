5e3a95adf2f96200660c6a0da72e29012a7a612b
Klinjin <zhenyujin@ucsb.edu>
2022-12-13 22:04:10 -0800

    zip argument #1 must support iteration

--- igm_emulator/hmc/nn_hmc_3d.py
+++ igm_emulator/hmc/nn_hmc_3d.py
@@ -48,7 +48,7 @@ class NN_HMC:
 
     def theta_to_x(self,theta):
         x_astro = []
-        for theta_i, theta_range in zip(theta, self.theta_ranges):
+        for theta_i, theta_range in zip(list(theta), self.theta_ranges):
             x_astro.append(jax.scipy.special.logit(
                 jnp.clip((theta_i - theta_range[0]) / (theta_range[1] - theta_range[0]),
                          a_min=1e-7, a_max=1.0 - 1e-7)))
