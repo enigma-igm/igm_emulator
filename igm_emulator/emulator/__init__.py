d9e5a90f391da2975d3b7acd4d1cedccb3589bc7
Klinjin <zhenyujin@ucsb.edu>
2023-08-10 14:49:11 -0700

    clear emulator init

--- igm_emulator/emulator/__init__.py
+++
@@ -1,9 +0,0 @@
-"""emulator - NN emulator for lyman-alpha auto-correlation function.
-
-Straightforward supervised learning (SL) Python library.
-It provides all the functions needed to do Neural Network learning and applicationrn
-It's backed by the JAX library and the Haiku framework.
-"""
-from .emulator_run import nn_emulator
-from .haiku_custom_forward import small_bin_bool, loss_fn, accuracy, custom_forward
-from .emulator_train import train_loop
