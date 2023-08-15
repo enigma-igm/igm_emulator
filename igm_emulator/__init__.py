a98a4d61eee6dce632e7d6da83f520b8f7025371
Klinjin <zhenyujin@ucsb.edu>
2023-08-15 12:52:39 -0700

    import nn_x

--- igm_emulator/__init__.py
+++ igm_emulator/__init__.py
@@ -7,3 +7,5 @@ It's backed by the JAX library and the Haiku framework.
 from igm_emulator.emulator.emulator_run import nn_emulator
 from igm_emulator.emulator.haiku_custom_forward import small_bin_bool, schedule_lr, loss_fn, accuracy, update, output_size, activation, l2, small_bin_bool
 #from igm_emulator.emulator.emulator_train import train_loop
+
+from igm_emulator.hmc.nn_hmc_3d_x import NN_HMC_X
