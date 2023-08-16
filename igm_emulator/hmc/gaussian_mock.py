c476032813135efa291020ceca6b7fb052cfaaae
Klinjin <zhenyujin@ucsb.edu>
2023-08-15 20:19:18 -0700

    add gaussian mock sampling

--- igm_emulator/hmc/gaussian_mock.py
+++ igm_emulator/hmc/gaussian_mock.py
@@ -7,7 +7,7 @@ for mock_idx in pbar(range(n_inference)):
     closest_temp_idx = np.argmin(np.abs(T0s - true_theta_sampled[mock_idx, 0]))
     closest_gamma_idx = np.argmin(np.abs(gammas - true_theta_sampled[mock_idx, 1]))
     closest_fobs_idx = np.argmin(np.abs(fobs - true_theta_sampled[mock_idx, 2]))
-    true_theta[mock_idx, :] = [fobs[closest_fobs_idx], T0s[closest_temp_idx], gammas[closest_gamma_idx]]
+
     model_name = f'likelihood_dicts_R_30000_nf_9_T{closest_temp_idx}_G{closest_gamma_idx}_SNR0_F{closest_fobs_idx}_ncovar_{n_covar}_P{n_path}{bin_label}.p'
     model_dict = dill.load(open(in_path + model_name, 'rb'))
     mean = model_dict['mean_data']
