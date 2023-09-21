fix model-covariance sampling in sample_mocks.py; run infer test with emu

igm_emulator/hmc/inference_test.py
@@ -105,6 +105,7 @@ if __name__ == '__main__':
 
     from sample_mocks import note
     out_path = '/mnt/quasar2/zhenyujin/igm_emulator/hmc/hmc_results/'
+    out_path_plot = f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{z_string}/emu_infer'
     save_name = f"{out_tag}_inference_{n_inference}_{note}_samples_{nn_x.num_samples}_chains_{nn_x.num_chains}_{var_tag}"
 
     key = random.PRNGKey(642)
@@ -154,7 +155,7 @@ if __name__ == '__main__':
                                        truths=np.array(true_theta[mock_idx, :]), truth_color='red', show_titles=True,
                                        quantiles=(0.16, 0.5, 0.84),title_kwargs={"fontsize": 15}, label_kwargs={'fontsize': 20},
                                        data_kwargs={'ms': 1.0, 'alpha': 0.1}, hist_kwargs=dict(density=True))
-            corner_fig.savefig(f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{z_string}/corner_T{closest_temp_idx}_G{closest_gamma_idx}_SNR{noise_idx}_F{closest_fobs_idx}_P{n_path}{bin_label}_mock_{mock_idx}_{var_tag}_{note}.png')
+            corner_fig.savefig(out_path_plot + f'corner_T{closest_temp_idx}_G{closest_gamma_idx}_SNR{noise_idx}_F{closest_fobs_idx}_P{n_path}{bin_label}_mock_{mock_idx}_{var_tag}_{note}.png')
     IPython.embed()
     #save HMC inference results
     with h5py.File(out_path + f'{save_name}.hdf5', 'a') as f:
@@ -211,7 +212,6 @@ if __name__ == '__main__':
     skew_ax.set_xlabel(r'$P_{{\rm true}}$', fontsize=16)
     skew_ax.set_ylabel(r'$P_{{\rm inf}}$', fontsize=16)
 
-    out_path_plot = f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{z_string}/'
     inference_fig.suptitle(f'{note}')
     inference_fig.savefig(out_path_plot + f'{save_name}.png')
     print(f'plot saved as: {save_name}.png')