a64dedd340a0390920af8ab42889c83a8f67c4c0
Klinjin <zhenyujin@ucsb.edu>
2023-09-21 16:36:07 -0700

    emulator infer test

--- igm_emulator/hmc/sample_mocks.py
+++ igm_emulator/hmc/sample_mocks.py
@@ -6,7 +6,7 @@ from progressbar import ProgressBar
 import igm_emulator as emu
 
 emu_test = False
-ngp = True #True: nearest grid point mocks/emulator; False: emulator
+ngp = False #True: nearest grid point mocks/emulator; False: emulator
 gaussian = True #True: gaussianized mocks/emulator; False: forward-modeled mocks
 
 if gaussian == False:
@@ -57,7 +57,6 @@ if gaussian:
             mean = model_dict['mean_data']
             cov = model_dict['covariance']
             if emu_test:
-                print('test emulator with ngp')
                 mean = emu.nn_emulator(best_params, true_theta[mock_idx, :])
                 cov = like_dict['covariance']
         elif emu_test:
