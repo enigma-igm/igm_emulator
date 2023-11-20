a2c4dfd10944e6ba7b9bd1291639e357d56230a8
Klinjin <klin.j@yahoo.com>
2023-11-19 21:07:30 -0800

    try inference test with optuna tuned emulator

--- igm_emulator/__init__.py
+++ igm_emulator/__init__.py
@@ -4,7 +4,7 @@ Straightforward supervised learning (SL) Python library.
 It provides all the functions needed to do Neural Network learning and applicationrn
 It's backed by the JAX library and the Haiku framework.
 """
-from igm_emulator.emulator.emulator_apply import nn_emulator, small_bin_bool
+from igm_emulator.emulator.emulator_apply import nn_emulator
 from igm_emulator.emulator.emulator_trainer import TrainerModule
 
 from igm_emulator.hmc.hmc_nn_inference import NN_HMC_X
