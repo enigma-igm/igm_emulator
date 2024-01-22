63d600a601103ec370754e1f158dd116b19f4b61
Klinjin <klin.j@yahoo.com>
2024-01-22 14:17:15 -0800

    fit plot legend setting

--- igm_emulator/hmc/hmc_nn_inference.py
+++ igm_emulator/hmc/hmc_nn_inference.py
@@ -352,13 +352,13 @@ class NN_HMC_X:
                           label='Covariance')
 
         fit_axis.text(
-            500, 0.03,
+            500, 0.015,
             'True Model \n' + r'$\langle F \rangle$' + f' = {np.round(theta_true[0], decimals=4)}' + f'\n $T_0$ = {int(theta_true[1])} K \n $\gamma$ = {np.round(theta_true[2], decimals=3)} \n',
             {'color': 'lightgreen', 'fontsize': 8},
         )
 
         fit_axis.text(
-            1000, 0.03,
+            1000, 0.015,
             'Inferred Model \n' + r'$\langle F \rangle$' + f' = {np.round(f_mcmc[0], decimals=4)}$^{{+{np.round(f_mcmc[1], decimals=4)}}}_{{-{np.round(f_mcmc[2], decimals=4)}}}$' +
             f'\n $T_0$ = {int(t_mcmc[0])}$^{{+{int(t_mcmc[1])}}}_{{-{int(t_mcmc[2])}}}$ K'
             f'\n ' + r'$\gamma$' + f' = {np.round(g_mcmc[0], decimals=3)}$^{{+{np.round(g_mcmc[1], decimals=3)}}}_{{-{np.round(g_mcmc[2], decimals=3)}}}$\n',
@@ -366,7 +366,7 @@ class NN_HMC_X:
         )
 
         fit_axis.text(
-            1510, 0.03,
+            1510, 0.015,
             tabulate([[r' $R_2$',
                        np.round(r2_score(model_corr, max_P_model), decimals=4)],
                       ['MSE',
