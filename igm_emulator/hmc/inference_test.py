6c4e88665575c4850d67c0e3fa6dac1e2ffcd000
Klinjin <klin.j@yahoo.com>
2024-01-22 14:34:39 -0800

    try 500 mocks and diff seeds (sampling + hmc)

--- igm_emulator/hmc/inference_test.py
+++ igm_emulator/hmc/inference_test.py
@@ -424,12 +424,12 @@ class INFERENCE_TEST():
 '''
 ##emulator -- emulator model test
 '''
-hmc_infer = INFERENCE_TEST(5.4,True,True,True,True)
+hmc_infer = INFERENCE_TEST(redshift=5.4,model_emulator_bool=True,gaussian_bool=True,ngp_bool=True,emu_test_bool=True,n_inference=500,key_sample=222,key_hmc=42)
 
 '''
 ##forward mocks -- emulator model
 '''
-#hmc_infer = INFERENCE_TEST(5.4,True,False,True,False)
+#hmc_infer = INFERENCE_TEST(redshift=5.4,model_emulator_bool=True,gaussian_bool=False,ngp_bool=True,emu_test_bool=False,n_inference=100)
 
 '''
 ##gaussian mocks -- NGP model
