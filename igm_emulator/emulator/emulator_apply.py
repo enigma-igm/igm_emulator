286d7d6f87449ed6616b384d942f2689f50c4ea4
Klinjin <klin.j@yahoo.com>
2024-01-24 11:51:36 -0800

    train emulator on smaller 100 trainset and save plot in emu_apply

--- igm_emulator/emulator/emulator_apply.py
+++ igm_emulator/emulator/emulator_apply.py
@@ -70,10 +70,9 @@ def nn_emulator(best_params_function, theta_linda):
 Check if jvmap works
 '''
 if __name__ == '__main__':
+    redshift = 5.4
     trainer.train_loop(True)
     trainer.save_training_info(redshift)
-    del trainer
-
     in_path_hdf5 = os.path.expanduser('~') + '/igm_emulator/igm_emulator/emulator/best_params/'
     best_params = dill.load(open(in_path_hdf5 + f'{trainer.out_tag}_{trainer.var_tag}_best_param.p', 'rb'))  # changed to optuna tuned best param
     test_preds = nn_emulator(best_params, X_test_og)
