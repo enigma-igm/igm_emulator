565583f325f24abd6be45f5399470a9914486dfc
Klinjin <klin.j@yahoo.com>
2024-02-14 14:21:02 -0800

    mcmc = MCMC(nuts_kernel, num_warmup=self.num_warmup, num_samples=2000, num_chains=8), target_accept_prob=0.99

--- igm_emulator/hmc/hmc_nn_inference.py
+++ igm_emulator/hmc/hmc_nn_inference.py
@@ -45,10 +45,11 @@ plt.rcParams.update(plt_params)
 #running everything in dimensionless parameter space (x)
 class NN_HMC_X:
     def __init__(self, vbins, best_params, T0s, gammas, fobs, dense_mass=True,
-                 max_tree_depth= (8,10), #10
+                 max_tree_depth= 12, #(8,10), #10
                  num_warmup=1000,
-                 num_samples=1000,
-                 num_chains=4):
+                 num_samples= 2000, #1000,
+                 num_chains= 8 #4
+                 ):
         '''
         Args:
             vbins: velocity bins
