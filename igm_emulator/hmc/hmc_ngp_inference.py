88ff164a162cc5f3993764871f98147475cb55dd
Klinjin <klin.j@yahoo.com>
2024-02-16 13:47:54 -0800

    plot subtraction

--- igm_emulator/hmc/hmc_ngp_inference.py
+++ igm_emulator/hmc/hmc_ngp_inference.py
@@ -230,7 +230,7 @@ if __name__ == '__main__':
     plt.imshow(chi_grid_ngp - chi_grid_nn, extent=[f_grid.min(), f_grid.max(), g_grid.min(), g_grid.max()],
                origin='lower',
                aspect='auto')
-    plt.colorbar(label='Chi NGP-NN')
+    plt.colorbar(label='Chi NGP-NN', cmap='coolwarm', vmin=0, vmax=0.3)
     plt.xlabel('f_grid')
     plt.ylabel('g_grid')
     plt.title('Color plot of Chi NGP-NN')
