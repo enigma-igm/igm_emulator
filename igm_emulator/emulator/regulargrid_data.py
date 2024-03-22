29c44d3eefa1ad9566474ded90f88565e131515d
Klinjin <klin.j@yahoo.com>
2024-03-22 13:40:35 -0700

    plot histogram of delta_v_nn for different subset

--- igm_emulator/emulator/regulargrid_data.py
+++ igm_emulator/emulator/regulargrid_data.py
@@ -264,7 +264,7 @@ def random_split(seed, all_data_, plot_bool = False):
 
 # Different sampling methods
 #final_samples, models, testing_param, testing_corr, vali_param, vali_corr, seed = regular_grid(plot_bool=True)
-sparce_samples, _, _, _, err_vali_param, err_vali_corr, seed_err = regular_grid(n_f=4,n_t=7,n_g=4, seed=33,plot_bool=False)
+sparce_samples, _, _, _, err_vali_param, err_vali_corr, seed_err = regular_grid(n_f=4,n_t=7,n_g=4, seed=20,plot_bool=False)
 final_samples, models, testing_param, testing_corr, vali_param, vali_corr, seed = random_split(seed=55,all_data_=sparce_samples,plot_bool=True)
 
 dir = '/home/zhenyujin/igm_emulator/igm_emulator/emulator/GRID'
