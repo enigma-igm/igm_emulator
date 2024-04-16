f6a50a5da36cdb207a253f9562c4b5f5193604d4
Klinjin <klin.j@yahoo.com>
2024-04-16 16:20:00 -0700

    fixed plot setting for paper

--- igm_emulator/hmc/hmc_ngp_inference.py
+++ igm_emulator/hmc/hmc_ngp_inference.py
@@ -98,62 +98,62 @@ if __name__ == '__main__':
     n_temps = len(t_0s)
     n_gammas = len(gammas)
     n_f = len(fobs)
-
-    noise_idx = 0
-    in_name_new_params = f'new_covariances_dict_R_30000_nf_9_ncovar_{n_covar}_' \
-                         f'P{skewers_per_data}{bin_label}_params.p'
-    new_param_dict = dill.load(open(in_path_molly + in_name_new_params, 'rb'))
-    new_temps = new_param_dict['new_temps']
-    new_gammas = new_param_dict['new_gammas']
-    new_fobs = new_param_dict['new_fobs']
-
-    n_new_t = (len(new_temps) - 1)/(len(t_0s) - 1) - 1
-    n_new_g = (len(new_gammas) - 1)/(len(gammas) - 1) - 1
-    n_new_f = (len(new_fobs) - 1)/(len(fobs) - 1) - 1
-    new_models_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs), len(v_bins)])
-    new_covariances_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs), len(v_bins),len(v_bins)])
-    new_log_dets_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs)])
-
-
-    for old_t_below_idx in range(n_temps - 1):
-        print(f'{old_t_below_idx / (n_temps - 1) * 100}%')
-        for old_g_below_idx in range(n_gammas - 1):
-            for old_f_below_idx in range(n_f - 1):
-                fine_dict_in_name = f'new_covariances_dict_R_{int(R_value)}_nf_{n_f}_T{old_t_below_idx}_' \
-                                    f'G{old_g_below_idx}_SNR0_F{old_f_below_idx}_ncovar_{n_covar}_' \
-                                    f'P{skewers_per_data}{bin_label}.p'
-                fine_dict = dill.load(open(in_path_molly + fine_dict_in_name, 'rb'))
-                new_temps_small = fine_dict['new_temps']
-                new_gammas_small = fine_dict['new_gammas']
-                new_fobs_small = fine_dict['new_fobs']
-                new_models_small = fine_dict['new_models']
-                new_covariances_small = fine_dict['new_covariances']
-                new_log_dets_small = fine_dict['new_log_dets']
-                if old_t_below_idx == n_temps - 2:
-                    added_t_range = n_new_t + 2
-                else:
-                    added_t_range = n_new_t + 1
-                if old_g_below_idx == n_gammas - 2:
-                    added_g_range = n_new_g + 2
-                else:
-                    added_g_range = n_new_g + 1
-                if old_f_below_idx == n_f - 2:
-                    added_f_range = n_new_f + 2
-                else:
-                    added_f_range = n_new_f + 1
-                # print(added_f_range)
-                for added_t_idx in range(int(added_t_range)):
-                    for added_g_idx in range(int(added_g_range)):
-                        for added_f_idx in range(int(added_f_range)):
-                            final_t_idx = int((old_t_below_idx * (n_new_t + 1)) + added_t_idx)
-                            final_g_idx = int((old_g_below_idx * (n_new_g + 1)) + added_g_idx)
-                            final_f_idx = int((old_f_below_idx * (n_new_f + 1)) + added_f_idx)
-                            new_models_np[final_t_idx, final_g_idx, final_f_idx, :] = new_models_small[added_t_idx, added_g_idx, added_f_idx, :]
-                            new_covariances_np[final_t_idx, final_g_idx, final_f_idx, :, :] = new_covariances_small[added_t_idx, added_g_idx, added_f_idx, :, :]
-                            new_log_dets_np[final_t_idx, final_g_idx, final_f_idx] = new_log_dets_small[added_t_idx, added_g_idx, added_f_idx]
-    new_models = jnp.array(new_models_np)
-    new_covariances = jnp.array(new_covariances_np)
-    new_log_dets = jnp.array(new_log_dets_np)
+    #
+    # noise_idx = 0
+    # in_name_new_params = f'new_covariances_dict_R_30000_nf_9_ncovar_{n_covar}_' \
+    #                      f'P{skewers_per_data}{bin_label}_params.p'
+    # new_param_dict = dill.load(open(in_path_molly + in_name_new_params, 'rb'))
+    # new_temps = new_param_dict['new_temps']
+    # new_gammas = new_param_dict['new_gammas']
+    # new_fobs = new_param_dict['new_fobs']
+    #
+    # n_new_t = (len(new_temps) - 1)/(len(t_0s) - 1) - 1
+    # n_new_g = (len(new_gammas) - 1)/(len(gammas) - 1) - 1
+    # n_new_f = (len(new_fobs) - 1)/(len(fobs) - 1) - 1
+    # new_models_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs), len(v_bins)])
+    # new_covariances_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs), len(v_bins),len(v_bins)])
+    # new_log_dets_np = np.empty([len(new_temps), len(new_gammas), len(new_fobs)])
+    #
+    #
+    # for old_t_below_idx in range(n_temps - 1):
+    #     print(f'{old_t_below_idx / (n_temps - 1) * 100}%')
+    #     for old_g_below_idx in range(n_gammas - 1):
+    #         for old_f_below_idx in range(n_f - 1):
+    #             fine_dict_in_name = f'new_covariances_dict_R_{int(R_value)}_nf_{n_f}_T{old_t_below_idx}_' \
+    #                                 f'G{old_g_below_idx}_SNR0_F{old_f_below_idx}_ncovar_{n_covar}_' \
+    #                                 f'P{skewers_per_data}{bin_label}.p'
+    #             fine_dict = dill.load(open(in_path_molly + fine_dict_in_name, 'rb'))
+    #             new_temps_small = fine_dict['new_temps']
+    #             new_gammas_small = fine_dict['new_gammas']
+    #             new_fobs_small = fine_dict['new_fobs']
+    #             new_models_small = fine_dict['new_models']
+    #             new_covariances_small = fine_dict['new_covariances']
+    #             new_log_dets_small = fine_dict['new_log_dets']
+    #             if old_t_below_idx == n_temps - 2:
+    #                 added_t_range = n_new_t + 2
+    #             else:
+    #                 added_t_range = n_new_t + 1
+    #             if old_g_below_idx == n_gammas - 2:
+    #                 added_g_range = n_new_g + 2
+    #             else:
+    #                 added_g_range = n_new_g + 1
+    #             if old_f_below_idx == n_f - 2:
+    #                 added_f_range = n_new_f + 2
+    #             else:
+    #                 added_f_range = n_new_f + 1
+    #             # print(added_f_range)
+    #             for added_t_idx in range(int(added_t_range)):
+    #                 for added_g_idx in range(int(added_g_range)):
+    #                     for added_f_idx in range(int(added_f_range)):
+    #                         final_t_idx = int((old_t_below_idx * (n_new_t + 1)) + added_t_idx)
+    #                         final_g_idx = int((old_g_below_idx * (n_new_g + 1)) + added_g_idx)
+    #                         final_f_idx = int((old_f_below_idx * (n_new_f + 1)) + added_f_idx)
+    #                         new_models_np[final_t_idx, final_g_idx, final_f_idx, :] = new_models_small[added_t_idx, added_g_idx, added_f_idx, :]
+    #                         new_covariances_np[final_t_idx, final_g_idx, final_f_idx, :, :] = new_covariances_small[added_t_idx, added_g_idx, added_f_idx, :, :]
+    #                         new_log_dets_np[final_t_idx, final_g_idx, final_f_idx] = new_log_dets_small[added_t_idx, added_g_idx, added_f_idx]
+    # new_models = jnp.array(new_models_np)
+    # new_covariances = jnp.array(new_covariances_np)
+    # new_log_dets = jnp.array(new_log_dets_np)
 
     T0_idx = 0  # 0-14
     g_idx = 5  # 0-8
@@ -176,23 +176,23 @@ if __name__ == '__main__':
     '''
     NGP HMC 
     '''
-    # Define the NGP inference
-    hmc_ngp = HMC_NGP(v_bins, new_temps, new_gammas, new_fobs, new_models, new_covariances, new_log_dets)
-    cov, log_det = hmc_ngp.get_covariance_log_determinant_nearest_fine(theta_true)
-    x_out, theta_out, losses = hmc_ngp.fit_one(flux, cov)
-    print(f'NGP [fobs, T0, gamma] ranges: {hmc_ngp.theta_ranges}')
-    print(np.argmin(np.abs(hmc_ngp.T0s - theta_true[1])),
-          np.argmin(np.abs(hmc_ngp.gammas - theta_true[2])),
-          np.argmin(np.abs(hmc_ngp.fobs - theta_true[0])))
-
-    key, subkey = random.split(key)
-    x_samples_ngp, theta_samples_ngp, lnP_ngp, neff_ngp, neff_mean_ngp, sec_per_neff_ngp, ms_per_step_ngp, r_hat_ngp, r_hat_mean_ngp, \
-        hmc_num_steps_ngp, hmc_tree_depth_ngp, total_time_ngp = hmc_ngp.mcmc_one(subkey, x_out, flux, cov, report=True)
-    hmc_ngp.corner_plot(zstr, theta_samples_ngp, x_samples_ngp, theta_out, save_str='ngp_hmc_test')
-    hmc_ngp.fit_plot(zstr, theta_samples_ngp, lnP_ngp, theta_out, model_corr=model, mock_corr=flux, covariance=cov,
-                     save_bool=True, save_str='ngp_hmc_test')
-    _, _, _, _, logP_grid_ngp, chi_grid_ngp = hmc_ngp.explore_logP_plot(zstr, theta_true=theta_out, flux=flux,
-                                                                        covar=cov, fix='t', save_str='ngp_hmc_test')
+    # # Define the NGP inference
+    # hmc_ngp = HMC_NGP(v_bins, new_temps, new_gammas, new_fobs, new_models, new_covariances, new_log_dets)
+    # cov, log_det = hmc_ngp.get_covariance_log_determinant_nearest_fine(theta_true)
+    # x_out, theta_out, losses = hmc_ngp.fit_one(flux, cov)
+    # print(f'NGP [fobs, T0, gamma] ranges: {hmc_ngp.theta_ranges}')
+    # print(np.argmin(np.abs(hmc_ngp.T0s - theta_true[1])),
+    #       np.argmin(np.abs(hmc_ngp.gammas - theta_true[2])),
+    #       np.argmin(np.abs(hmc_ngp.fobs - theta_true[0])))
+    #
+    # key, subkey = random.split(key)
+    # x_samples_ngp, theta_samples_ngp, lnP_ngp, neff_ngp, neff_mean_ngp, sec_per_neff_ngp, ms_per_step_ngp, r_hat_ngp, r_hat_mean_ngp, \
+    #     hmc_num_steps_ngp, hmc_tree_depth_ngp, total_time_ngp = hmc_ngp.mcmc_one(subkey, x_out, flux, cov, report=True)
+    # hmc_ngp.corner_plot(zstr, theta_samples_ngp, x_samples_ngp, theta_out, save_str='ngp_hmc_test')
+    # hmc_ngp.fit_plot(zstr, theta_samples_ngp, lnP_ngp, theta_out, model_corr=model, mock_corr=flux, covariance=cov,
+    #                  save_bool=True, save_str='ngp_hmc_test')
+    # _, _, _, _, logP_grid_ngp, chi_grid_ngp = hmc_ngp.explore_logP_plot(zstr, theta_true=theta_out, flux=flux,
+    #                                                                     covar=cov, fix='t', save_str='ngp_hmc_test')
 
     '''
     Emulator HMC
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     key, subkey = random.split(key)
     x_samples, theta_samples, lnP, neff, neff_mean, sec_per_neff, ms_per_step, r_hat, r_hat_mean, \
         hmc_num_steps, hmc_tree_depth, total_time = hmc_nn.mcmc_one(subkey, x_out, flux, cov, report=True)
-    hmc_nn.corner_plot(zstr, theta_samples, x_samples, theta_out, save_str=None, save_bool=False)
+    hmc_nn.corner_plot(zstr, theta_samples, x_samples, theta_out, save_str=None, save_bool=True)
     hmc_nn.fit_plot(zstr, theta_samples, lnP, theta_out, model_corr=model, mock_corr=flux, covariance=cov,
                     save_bool=True, save_str=None)
     fix, f_grid, t_grid, g_grid, logP_grid_nn, chi_grid_nn = hmc_nn.explore_logP_plot(zstr, theta_true=theta_out,
@@ -221,24 +221,24 @@ if __name__ == '__main__':
     '''
     Plot NGP and NN lnP and chi difference
     '''
-    plt.figure(figsize=(10, 8))
-    plt.imshow(logP_grid_nn-logP_grid_ngp, extent=[f_grid.min(), f_grid.max(), g_grid.min(), g_grid.max()], origin='lower',
-               aspect='auto', cmap='viridis',vmin=0, vmax=25)
-    plt.colorbar(label='lnP NN-NGP')
-    plt.xlabel('f_grid')
-    plt.ylabel('g_grid')
-    plt.title('Color plot of lnP NN-NGP')
-    plt.savefig(f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{zstr}/hmc/lnP_subtract_fix_{fix}_T{closest_temp_idx}_G{closest_gamma_idx}_F{closest_fobs_idx}.pdf')
-    plt.close()
-
-    plt.figure(figsize=(10, 8))
-    plt.imshow(chi_grid_ngp - chi_grid_nn, extent=[f_grid.min(), f_grid.max(), g_grid.min(), g_grid.max()],
-               origin='lower',
-               aspect='auto', cmap='viridis',vmin=0, vmax=0.3)
-    plt.colorbar(label='Chi NGP-NN')
-    plt.xlabel('f_grid')
-    plt.ylabel('g_grid')
-    plt.title('Color plot of Chi NGP-NN')
-    plt.savefig(
-        f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{zstr}/hmc/chi_subtract_fix_{fix}_T{closest_temp_idx}_G{closest_gamma_idx}_F{closest_fobs_idx}.pdf')
-    plt.close()
+    # plt.figure(figsize=(10, 8))
+    # plt.imshow(logP_grid_nn-logP_grid_ngp, extent=[f_grid.min(), f_grid.max(), g_grid.min(), g_grid.max()], origin='lower',
+    #            aspect='auto', cmap='viridis',vmin=0, vmax=25)
+    # plt.colorbar(label='lnP NN-NGP')
+    # plt.xlabel('f_grid')
+    # plt.ylabel('g_grid')
+    # plt.title('Color plot of lnP NN-NGP')
+    # plt.savefig(f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{zstr}/hmc/lnP_subtract_fix_{fix}_T{closest_temp_idx}_G{closest_gamma_idx}_F{closest_fobs_idx}.pdf')
+    # plt.close()
+    #
+    # plt.figure(figsize=(10, 8))
+    # plt.imshow(chi_grid_ngp - chi_grid_nn, extent=[f_grid.min(), f_grid.max(), g_grid.min(), g_grid.max()],
+    #            origin='lower',
+    #            aspect='auto', cmap='viridis',vmin=0, vmax=0.3)
+    # plt.colorbar(label='Chi NGP-NN')
+    # plt.xlabel('f_grid')
+    # plt.ylabel('g_grid')
+    # plt.title('Color plot of Chi NGP-NN')
+    # plt.savefig(
+    #     f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/plots/{zstr}/hmc/chi_subtract_fix_{fix}_T{closest_temp_idx}_G{closest_gamma_idx}_F{closest_fobs_idx}.pdf')
+    # plt.close()
