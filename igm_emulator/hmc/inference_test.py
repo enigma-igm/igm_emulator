9501a37cea0ad1931511b6daa1fa5f5b24d4586a
Klinjin <klin.j@yahoo.com>
2024-04-17 14:55:43 -0700

    fixed plot setting for paper in inference test z=5.4, nn_err=False

--- igm_emulator/hmc/inference_test.py
+++ igm_emulator/hmc/inference_test.py
@@ -86,10 +86,8 @@ class INFERENCE_TEST():
         self.var_tag = trainer.var_tag
         self.out_tag = trainer.out_tag
         self.best_params = dill.load(open(in_path_hdf5 + f'{self.out_tag}_{self.var_tag}_best_param.p', 'rb')) #changed to optuna tuned best param
-        #self.covar_nn = dill.load(open(in_path_hdf5 + f'{self.out_tag}_{self.var_tag}_covar_nn.p', 'rb'))
-        #self.err_nn = dill.load(open(in_path_hdf5 + f'{self.out_tag}_{self.var_tag}_err_nn.p', 'rb'))
-        self.covar_nn = dill.load(open('/mnt/quasar2/zhenyujin/igm_emulator/emulator/best_params/' + f'z55_train_55__err_v_221_mape_l2_0_perc_True_activation_tanh_covar_nn.p', 'rb'))
-        self.err_nn = dill.load(open('/mnt/quasar2/zhenyujin/igm_emulator/emulator/best_params/' + f'z55_train_55__err_v_221_mape_l2_0_perc_True_activation_tanh_err_nn.p', 'rb'))
+        self.covar_nn = dill.load(open(in_path_hdf5 + f'{self.out_tag}_{self.var_tag}_covar_nn.p', 'rb'))
+        self.err_nn = dill.load(open(in_path_hdf5 + f'{self.out_tag}_{self.var_tag}_err_nn.p', 'rb'))
         if self.ngp_bool:
             self.var_tag += '_NGP_model'
 
