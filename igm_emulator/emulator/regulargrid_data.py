42b27b44d1877ee5dca325b2cbd14baf5e5d8f97
Klinjin <klin.j@yahoo.com>
2024-04-22 15:08:24 -0700

    112: sampling contol test data size --> new naming method

--- igm_emulator/emulator/regulargrid_data.py
+++ igm_emulator/emulator/regulargrid_data.py
@@ -292,8 +292,8 @@ class DataSamplerModule:
             A = self.X_vali
             ax.scatter(A[:, 0], A[:, 1], A[:, 2], c='g', linewidth=0.2, label=f'validation data: {A.shape[0]}')
             T = self.X_test
-            ax.scatter(T[:, 0], T[:, 1], T[:, 2], c='k', linewidth=0.2, label=f'validation data: {T.shape[0]+T_err.shape[0]}')
             T_err = self.extra_test_err_param
+            ax.scatter(T[:, 0], T[:, 1], T[:, 2], c='k', linewidth=0.2, label=f'validation data: {T.shape[0]+T_err.shape[0]}')
             ax.scatter(T_err[:, 0], T_err[:, 1], T_err[:, 2], c='k', linewidth=0.2, alpha = 0.5)
             ax.set_xlabel(r'$<F>$')
             ax.set_ylabel(r'$T_0$')
