ba0059d0cf1467ec965f9423f7e35c2e083ccf2c
Klinjin <klin.j@yahoo.com>
2024-05-02 12:31:25 -0700

    n_test = 12, seed = 11 in z=5.4 emulator, infer central values for 2 mocks and mean

--- igm_emulator/hmc/hmc_run.py
+++ igm_emulator/hmc/hmc_run.py
@@ -118,19 +118,34 @@ if __name__ == '__main__':
                                    mock_corr=flux,
                                    covariance=new_covariance, save_str=f'central_mock_{mock_idx}', save_bool=True)
 
-        if compare:
-            in_path_model = f'/mnt/quasar2/mawolfson/correlation_funct/temp_gamma/final/{z_string}/final_135/'
-            molly_name = f'z54_data_nearest_model_set_bins_4_steps_48000_mcmc_inference_5_one_prior_T{T0_idx}_G{g_idx}_F{f_idx}_R_30000.hdf5'
-            molly_model = h5py.File(in_path_model + molly_name, 'r')
+    flux = mean_flux
+    x_opt, theta_opt, losses = nn_x.fit_one(flux, new_covariance)
+    x_samples, theta_samples, lnP, neff, neff_mean, sec_per_neff, ms_per_step, r_hat, r_hat_mean, \
+        hmc_num_steps, hmc_tree_depth, total_time = nn_x.mcmc_one(subkey, x_opt, flux, new_covariance, report=True)
+    f_mcmc, t_mcmc, g_mcmc = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+                                 zip(*np.percentile(theta_samples, [16, 50, 84], axis=0)))
+    nn_x.save_HMC(z_string,f_idx, T0_idx,g_idx, f_mcmc, t_mcmc, g_mcmc, x_samples, theta_samples,theta_true, lnP, neff, neff_mean, sec_per_neff, ms_per_step, r_hat, r_hat_mean,
+             hmc_num_steps, hmc_tree_depth, total_time,save_str=f'central_mean_model')
 
-            molly_sample = molly_model['samples'][mock_idx, :, :]
-            molly_flip = np.zeros(shape=molly_sample.shape)
-            molly_flip[:, 0] = molly_sample[:, 2]
-            molly_flip[:, 1] = molly_sample[:, 0]
-            molly_flip[:, 2] = molly_sample[:, 1]
-
-            t_molly, g_molly, f_molly = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
-                                            zip(*np.percentile(molly_sample, [16, 50, 84], axis=0)))
-            molly_infer, covar, log_det = get_model_covar_nearest([t_molly[0], g_molly[0], f_molly[0]])
-
-            corner.corner(molly_flip, levels=(0.68, 0.95), fig=corner_fig, color='blue',hist_kwargs=dict(density=True))
+    corner_fig = nn_x.corner_plot(z_string, theta_samples, x_samples, theta_true,
+                                    save_str=f'central_mean_model', save_bool=True)
+    fit_fig = nn_x.fit_plot(z_string=z_string, theta_samples=theta_samples, lnP=lnP,
+                               theta_true=theta_true, model_corr=mean_flux,
+                               mock_corr=flux,
+                               covariance=new_covariance, save_str=f'central_mean_model', save_bool=True)
+        #if compare:
+        #    in_path_model = f'/mnt/quasar2/mawolfson/correlation_funct/temp_gamma/final/{z_string}/final_135/'
+        #    molly_name = f'z54_data_nearest_model_set_bins_4_steps_48000_mcmc_inference_5_one_prior_T{T0_idx}_G{g_idx}_F{f_idx}_R_30000.hdf5'
+        #    molly_model = h5py.File(in_path_model + molly_name, 'r')
+        #
+        #    molly_sample = molly_model['samples'][mock_idx, :, :]
+        #    molly_flip = np.zeros(shape=molly_sample.shape)
+        #    molly_flip[:, 0] = molly_sample[:, 2]
+        #    molly_flip[:, 1] = molly_sample[:, 0]
+        #    molly_flip[:, 2] = molly_sample[:, 1]
+        #
+        #    t_molly, g_molly, f_molly = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+        #                                    zip(*np.percentile(molly_sample, [16, 50, 84], axis=0)))
+        #    molly_infer, covar, log_det = get_model_covar_nearest([t_molly[0], g_molly[0], f_molly[0]])
+        #
+        #    corner.corner(molly_flip, levels=(0.68, 0.95), fig=corner_fig, color='blue',hist_kwargs=dict(density=True))
