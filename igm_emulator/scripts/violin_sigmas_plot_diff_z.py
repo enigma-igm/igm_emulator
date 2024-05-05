86b10d9a9df17ec7becf50d0dc79e404beeee91e
Klinjin <klin.j@yahoo.com>
2024-05-04 17:30:36 -0700

    try plotting the violin figure

--- igm_emulator/scripts/violin_sigmas_plot_diff_z.py
+++ igm_emulator/scripts/violin_sigmas_plot_diff_z.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     in_path_read = f'/mnt/quasar2/zhenyujin/igm_emulator/hmc/hmc_results/central_models/'
     out_file_tag = f'hmc_inference_{int(n_inference)}'
     in_name_inference = f'{z_strings[0]}_{z_strings[-1]}_F{true_fobs_idx}_T0{true_temp_idx}_G{true_gamma_idx}_central_model_{out_file_tag}.hdf5'
-    with h5py.File(in_path_read + in_name_inference, 'r') as f:
+    with h5py.File(in_path_read + 'z54_z6_F4_T07_G4_central_model_hmc_inference_4000.hdf5', 'r') as f:
         samples_temp = f['samples_temp']
         samples_gamma = f['samples_gamma']
 
