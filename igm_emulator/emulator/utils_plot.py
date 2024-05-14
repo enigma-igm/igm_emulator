9c6bf545d717c568d14e557c502661794f283ddf
Klinjin <klin.j@yahoo.com>
2024-05-14 16:46:39 -0700

    test overplot remake -- chatbot

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -216,8 +216,8 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
         for col in range(3):
             if row == 2:
                 axs2 = fig2.add_subplot(grid[row, col])
-                axs2.set_yticks([])
                 axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
+                axs2.tick_params(axis='y', which='both',bottom=False,labelbottom=False)
             else:
                 axs2 = fig2.add_subplot(grid[row, col], sharex=fig2.add_subplot(grid[2, col]))
                 axs2.tick_params(axis='x', which='both',bottom=False,labelbottom=False)
