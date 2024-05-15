db85a763d3f9cc8c4188d10b433b11be1c7980e1
Klinjin <klin.j@yahoo.com>
2024-05-14 17:11:46 -0700

    test overplot remake -- autoscaley_on=False for last row

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -215,8 +215,8 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
     for row in range(3):
         for col in range(3):
             if row == 2:
-                axs2 = fig2.add_subplot(grid[row, col], autoscaley_on=False)
-                axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
+                axs2 = fig2.add_subplot(grid[row, col])
+                #axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
             else:
                 axs2 = fig2.add_subplot(grid[row, col], sharex=fig2.add_subplot(grid[2, col]))
                 axs2.tick_params(axis='x', which='both',bottom=False, labelbottom=False)
