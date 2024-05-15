5a6fbd02f3a68e955d61a9959d0c30eea3f6798a
Klinjin <klin.j@yahoo.com>
2024-05-14 17:31:53 -0700

    test overplot remake -- add_subplot no auto y axis for last row: axs2.axis["left"].set_visible(False)

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -202,8 +202,8 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
     '''
     ax = v_bins
     sample = 9  # number of functions plotted
-    fig2 = plt.figure(figsize=(x_size * 4., x_size * .65 * 2), constrained_layout=True, dpi=dpi_value)
-    grid = fig2.add_gridspec(nrows=3, ncols=3)
+    fig2, axes = plt.subplots(3, 3,figsize=(x_size * 4., x_size * .65 * 2), constrained_layout=True, dpi=dpi_value)
+    #grid = fig2.add_gridspec(nrows=3, ncols=3)
     fig2.set_constrained_layout_pads(
         w_pad=.025, h_pad=.025,
         hspace=0, wspace=0
@@ -218,7 +218,7 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
                 axs2 = fig2.add_subplot(grid[row, col])
                 axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
             else:
-                axs2 = fig2.add_subplot(grid[row, col], sharex=fig2.subplot(grid[2, col]))
+                axs2 = fig2.add_subplot(grid[row, col], sharex=axes[2, col])
                 axs2.tick_params(axis='x', which='both',bottom=False, labelbottom=False)
             i = 3*row+col
             axs2.plot(ax, Y_test[corr_idx[i]], label=r'$\xi_F$', c='r')
