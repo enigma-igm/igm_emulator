cd76093e010f9db50cd7dbe6fd4e835d1da6d3c3
Klinjin <klin.j@yahoo.com>
2024-05-14 17:32:18 -0700

    test overplot remake -- add_subplot no auto y axis for last row: axs2.axis["left"].set_visible(False)

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -203,7 +203,7 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
     ax = v_bins
     sample = 9  # number of functions plotted
     fig2, axes = plt.subplots(3, 3,figsize=(x_size * 4., x_size * .65 * 2), constrained_layout=True, dpi=dpi_value)
-    #grid = fig2.add_gridspec(nrows=3, ncols=3)
+    grid = fig2.add_gridspec(nrows=3, ncols=3)
     fig2.set_constrained_layout_pads(
         w_pad=.025, h_pad=.025,
         hspace=0, wspace=0
