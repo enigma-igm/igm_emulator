616f496d16ae86e49cc2382de352f500dd180d71
Klinjin <klin.j@yahoo.com>
2024-06-11 16:12:20 -0700

    z5.4 central model HMC fit plot fixed

--- igm_emulator/hmc/hmc_nn_inference.py
+++ igm_emulator/hmc/hmc_nn_inference.py
@@ -611,8 +611,7 @@ class NN_HMC_X:
                       path_effects=[pe.Stroke(linewidth=1.25, foreground='k'), pe.Normal()])
         fit_axis.plot(self.vbins, model_corr, c="green", ls='--', label='True Model', zorder=2, lw=1.75,
                       path_effects=[pe.Stroke(linewidth=2, foreground='k'), pe.Normal()])
-        fit_axis.scatter(self.vbins, mock_corr, c="k", zorder=3, s=5)
-
+        fit_axis.scatter(self.vbins, mock_corr, c="k", zorder=3, s=2)
         fit_axis.plot(self.vbins, max_P_model, c="gold", label='Max Probability Model', zorder=4, lw=1,
                       path_effects=[pe.Stroke(linewidth=1.25, foreground='k'), pe.Normal()])
         fit_axis.errorbar(self.vbins, mock_corr,
