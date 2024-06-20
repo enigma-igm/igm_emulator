bcd7161b5637cbe4eb9dfcd9fa26180086c226ef
Klinjin <klin.j@yahoo.com>
2024-06-20 11:56:26 -0700

    z5.4 add test overplot +residual

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -227,13 +227,12 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
             if i == 0:
                 axs2.plot(ax, Y_test[corr_idx[i]], label=r'$\xi_F$', c='r')
                 axs2.plot(ax, test_preds[corr_idx[i]], label=r'Ly$\alpha$ Emulator', c='b', linestyle='--')
-                new_ax = axs2.add_axes((.1, .1, .8, .2))
+                #new_ax = axs2.add_axes((.1, .1, .8, .2))
+                new_ax = plt.subplot(2,1,sharex=axs2)
                 new_ax.plot(ax, (Y_test[corr_idx[i]]-test_preds[corr_idx[i]])/Y_test[corr_idx[i]], label='Percentage Residual',c='b')
             else:
                 axs2.plot(ax, Y_test[corr_idx[i]], c='r')
                 axs2.plot(ax, test_preds[corr_idx[i]], c='b', linestyle='--')
-                new_ax = axs2.add_axes((.1, .1, .8, .2))
-                new_ax.plot(ax, (Y_test[corr_idx[i]] - test_preds[corr_idx[i]]) / Y_test[corr_idx[i]], c='b')
             if col == 0:
                 axs2.set_ylabel(r"$\xi_F$")
             else:
