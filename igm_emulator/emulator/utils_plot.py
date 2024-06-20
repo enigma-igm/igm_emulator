e62606033c9e1b31c58493bc6f284d4acac6f92b
Klinjin <klin.j@yahoo.com>
2024-06-20 12:36:14 -0700

    z5.4 add test overplot +residual

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -222,7 +222,7 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
             if row == 2:
                 axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
             else:
-                axs2.sharex(subfigs[2, col].axes) #[2, col]
+                axs2.sharex(subfigs[2, col].axes[0]) #[2, col]
                 axs2.tick_params(axis='x', which='both',bottom=False, labelbottom=False)
             axs2.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
             if i == 0:
