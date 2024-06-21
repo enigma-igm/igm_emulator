c6f69e330c193d33e01b7427c38f2a2d7f80cd54
Klinjin <klin.j@yahoo.com>
2024-06-21 14:51:54 -0700

    z5.4 test pver plot

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -224,6 +224,15 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
     test_preds = test_preds*stdY+meanY
     Y_test = Y_test*stdY+meanY
     X_test = X_test*stdX+meanX
+
+    # Get the indices that would sort X_test[:,0]
+    sort_indices = np.argsort(X_test[:, 0])
+
+    # Use these indices to sort X_test, Y_test, and test_preds
+    X_test_sorted = X_test[sort_indices]
+    Y_test_sorted = Y_test[sort_indices]
+    test_preds_sorted = test_preds[sort_indices]
+
     for row in range(3):
         for col in range(3):
             i = 3 * row + col
@@ -238,39 +247,43 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
                     axs2.set_xlabel(r'Velocity [$km s^{-1}$]')
             else:
                 if residual_plot:
-                    shared_ax = new_ax_list[2, col]
+                    shared_ax_x = new_ax_list[2, col]
+                    shared_new_ax_y = new_ax_list[row, 0]
                     new_ax.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
+                    new_ax.sharey(shared_new_ax_y)
                 else:
-                    shared_ax = axs2_list[2, col]
+                    shared_ax_x = axs2_list[2, col]
                     axs2.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
-                axs2.sharex(shared_ax) #[2, col]
+                shared_ax_y = axs2_list[row, 0]
+                axs2.sharex(shared_ax_x) #[2, col]
+                axs2.sharey(shared_ax_y) #[row, 0]
             axs2.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
             axs2.yaxis.major.formatter.set_powerlimits((0, 0))
             if i == 0:
-                axs2.plot(ax, Y_test[corr_idx[i]], label=r'$\xi_F$', c='r')
-                axs2.plot(ax, test_preds[corr_idx[i]], label=r'Ly$\alpha$ Emulator', c='b', linestyle='--')
+                axs2.plot(ax, Y_test_sorted[corr_idx[i]], label=r'$\xi_F$', c='r')
+                axs2.plot(ax, test_preds_sorted[corr_idx[i]], label=r'Ly$\alpha$ Emulator', c='b', linestyle='--')
                 if residual_plot:
-                    new_ax.plot(ax, (Y_test[corr_idx[i]]-test_preds[corr_idx[i]])/Y_test[corr_idx[i]]*100, label='Percentage Residual',alpha = 0.5, c='k')
+                    new_ax.plot(ax, (Y_test_sorted[corr_idx[i]]-test_preds_sorted[corr_idx[i]])/Y_test_sorted[corr_idx[i]]*100, label='Percentage Residual',alpha = 0.5, c='k')
             else:
-                axs2.plot(ax, Y_test[corr_idx[i]], c='r')
-                axs2.plot(ax, test_preds[corr_idx[i]], c='b', linestyle='--')
+                axs2.plot(ax, Y_test_sorted[corr_idx[i]], c='r')
+                axs2.plot(ax, test_preds_sorted[corr_idx[i]], c='b', linestyle='--')
                 if residual_plot:
-                    new_ax.plot(ax, (Y_test[corr_idx[i]]-test_preds[corr_idx[i]])/Y_test[corr_idx[i]]*100, alpha = 0.5, c='k')
+                    new_ax.plot(ax, (Y_test_sorted[corr_idx[i]]-test_preds_sorted[corr_idx[i]])/Y_test_sorted[corr_idx[i]]*100, alpha = 0.5, c='k')
             new_ax.set_ylim(-1, 1)
             if col == 0:
                 axs2.set_ylabel(r"$\xi_F$")
                 if residual_plot:
                     new_ax.set_ylabel('[%]')
             else:
-                axs2.tick_params(axis='y', which='both', direction='in', pad=-20, length=2)
+                axs2.tick_params(axis='y', which='both', direction='in', length=2, labelleft=False)
                 if residual_plot:
-                    new_ax.tick_params(axis='y', which='major', direction='in', pad=-20, length=2)
+                    new_ax.tick_params(axis='y', which='both', direction='in', length=2, labelleft=False)
             #yticks = ticker.MaxNLocator(nbins=5)
             #axs2.yaxis.set_major_locator(yticks)
 
-            axs2.text(0.2, 0.4,'$<F>$='f'{X_test[corr_idx[i], 0]:.4f},'
-                    r'$T_0$='f'{X_test[corr_idx[i], 1]:.0f},'
-                    r'$\gamma$='f'{X_test[corr_idx[i], 2]:.2f}', transform=axs2.transAxes,fontsize=7)
+            axs2.text(0.2, 0.4,'$<F>$='f'{X_test_sorted[corr_idx[i], 0]:.4f},'
+                    r'$T_0$='f'{X_test_sorted[corr_idx[i], 1]:.0f},'
+                    r'$\gamma$='f'{X_test_sorted[corr_idx[i], 2]:.2f}', transform=axs2.transAxes,fontsize=7)
             axs2.legend(fontsize=7, loc='upper right')
             if residual_plot:
                 new_ax.legend(fontsize=7, loc='lower right')
