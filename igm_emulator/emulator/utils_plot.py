f1acbce2968e127747b058621cb9a77f19357b2d
Klinjin <klin.j@yahoo.com>
2024-06-21 17:05:56 -0700

    z5.4 test pver plottry to align

--- igm_emulator/emulator/utils_plot.py
+++ igm_emulator/emulator/utils_plot.py
@@ -273,13 +273,14 @@ def test_overplot(test_preds, Y_test, X_test, meanX,stdX,meanY,stdY, out_tag, va
                 if residual_plot:
                     new_ax.plot(ax, (Y_test_sorted[i]-test_preds_sorted[i])/Y_test_sorted[i]*100, label='Percentage Residual',alpha = 0.5, c='k')
                     new_ax.legend(fontsize=7, loc='lower right')
+                    new_ax.fill_between(np.arange(ax[0]-2000,ax[-1]+2000), -0.5, 0.5, color='r', alpha=0.1)
             else:
                 axs2.plot(ax, Y_test_sorted[i], c='r')
                 axs2.plot(ax, test_preds_sorted[i], c='b', linestyle='--')
                 if residual_plot:
                     new_ax.plot(ax, (Y_test_sorted[i]-test_preds_sorted[i])/Y_test_sorted[i]*100, alpha = 0.5, c='k')
-                    new_ax.fill_between(np.arange(ax[0]-20,ax[-1]+20), -0.5, 0.5, color='r', alpha=0.1)
-            axs2.set_xlim(ax[0]-10, ax[-1]+10)
+                    new_ax.fill_between(np.arange(ax[0]-2000,ax[-1]+2000), -0.5, 0.5, color='r', alpha=0.1)
+            axs2.set_xlim(ax[0]-100, ax[-1]+100)
             new_ax.set_ylim(-1, 1)
             if col == 0:
                 axs2.set_ylabel(r"$\xi_F$")
