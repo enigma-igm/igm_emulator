a39c22eaca8a86366d979663b5d5775c360f8352
Klinjin <klin.j@yahoo.com>
2024-06-25 12:02:28 -0700

    try interactive redshift+hmc settings

--- igm_emulator/lya_thermal_emulator.py
+++ igm_emulator/lya_thermal_emulator.py
@@ -1,9 +1,16 @@
+import numpy as np
 def main():
     redshift = input("Emulate at Redshift ([5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0]): ")
     small_scale = input("Less velocity bins of Ly-a forest ([True, False]): ")
     n_testing = int(input("Extra testing data number: "))
 
-    return redshift, small_scale, n_testing
+    # Convert the input to numpy.float64
+    try:
+        redshift_float64 = np.float64(redshift)
+        print(f"The value you entered is {redshift_float64} with type {type(redshift_float64)}")
+    except ValueError:
+        print("Invalid input. Please enter a valid redshift.")
+    return redshift_float64, small_scale, n_testing
 
 redshift, small_scale, n_testing = main()
 
