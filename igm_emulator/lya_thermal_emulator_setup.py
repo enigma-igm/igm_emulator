fbb50364e3d05e2c2755c3d9f6c98f7fa612815a
Klinjin <klin.j@yahoo.com>
2024-06-26 16:21:31 -0700

    try interactive emulator with updated testplot set y ticks

--- igm_emulator/lya_thermal_emulator_setup.py
+++ igm_emulator/lya_thermal_emulator_setup.py
@@ -2,8 +2,8 @@ import numpy as np
 import subprocess
 def main():
     redshift = input("Emulate at Redshift ([5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0]): ")
-    small_scale = input("Less velocity bins of Ly-a forest ([True, False]): ")
-    n_testing = int(input("Extra testing data number for NN error estimation: "))
+    small_scale = input("Less velocity bins of Ly-a forest (Y/N): ") == 'Y'
+    n_testing = int(input(r"Extra testing data number for NN error estimation ($\leq 1115$): "))
 
     # Convert the input to numpy.float64
     try:
@@ -16,8 +16,8 @@ if __name__ != "__main__":
     redshift, small_scale, n_testing = main()
 
 if __name__ == "__main__":
-    emulator_tune = input('Tune hyperparameters of the emulator at this redshift (Y/N)?') == 'Y'
-    emulator_train = input('Show training of best-hparam emulator with plots (Y/N)?') == 'Y'
+    emulator_tune = input('Tune hyperparameters of the emulator at this redshift ~10 min (Y/N)?') == 'Y'
+    emulator_train = input('Show training of best-hparam emulator with plots ~5 sec (Y/N)?') == 'Y'
 
     if emulator_tune:
         print("Running 'python3 hparam_tuning'")
