69b89a31595377de8c49d8407a3a85a9ba0fd10b
Klinjin <klin.j@yahoo.com>
2024-11-18 19:22:52 -0800

    plot mean models with t f g, change f width

--- igm_emulator/scripts/violin_T_g_f_plot_diff_z_mean_model.py
+++ igm_emulator/scripts/violin_T_g_f_plot_diff_z_mean_model.py
@@ -389,7 +389,7 @@ if __name__ == '__main__':
 
         temp_axis.set_ylim([min_temps[-1], max_temps[0]])
         gamma_axis.set_ylim([min_gammas[0], max_gammas[-1]])
-        f_axis.set_ylim([min_f[-1], max_f[1]])
+        f_axis.set_ylim([0.0006, 0.085])
 
         temp_axis.tick_params(
             axis='x',
